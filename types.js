b6db1f17045a8bfc54c6e5fde2a915808bc8d05a
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-11 08:54:29 -0700

    first commit

---
+++ types.js
@@ -0,0 +1,10 @@
+console.log("hello from ts");
+var myString;
+var myNum;
+var myBool;
+myString = "333";
+myNum = 22;
+myBool = 1;
+console.log(myString);
+console.log(myNum);
+console.log(myBool);
---
+++ types.ts
@@ -0,0 +1,13 @@
+console.log("hello from ts");
+
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+
+myString = "333";
+myNum = 22;
+myBool = 1;
+
+console.log(myString);
+console.log(myNum);
+console.log(myBool);
